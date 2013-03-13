5759bb0c346bfd6a9973ff43a8e0973048173e35
João Ferreira Loff <jfloff@gmail.com>
2013-03-13 12:30:46 +0000

    Fixed problem with nulls

--- projecto/src/ist/meic/pa/CheckerTranslator.java
+++ projecto/src/ist/meic/pa/CheckerTranslator.java
@@ -17,38 +17,29 @@ public class CheckerTranslator implements Translator {
     public void checkerField(CtClass ctClass) throws NotFoundException, CannotCompileException {
 	    final String template =
 	        "{" +
-	        "  ist.meic.pa.CheckerTranslator.checkField(%s);" + // Tirar isto daqui??
 	        "  $0.%s = $1;" +
+	        "  ist.meic.pa.CheckerTranslator.checkField(%s);" +
 	        "}";
 
-	    final CtClass ctClassTmp = ctClass;
-
+	    // PERCORRER O RESTO§
 	    for (CtMethod ctMethod : ctClass.getDeclaredMethods()){
 	        ctMethod.instrument(new ExprEditor() {
 	            public void edit(FieldAccess fa) throws CannotCompileException {
 	                if (fa.isWriter()) {
-	                    String name = fa.getFieldName();
-	                    // falta tratar do null
-	                    String expr = parseAssertion(ctClassTmp, name);
-	                    String formatTemplate = String.format(template, expr, name);
-	                    System.out.println(formatTemplate);
-	                    fa.replace(formatTemplate);
+						try{
+							String fieldName = fa.getFieldName();
+	                    	CtField field = fa.getField();
+		                    if(field.hasAnnotation(Assertion.class)){
+		                    	Assertion a = (Assertion) field.getAnnotation(Assertion.class);
+			                    String formatTemplate = String.format(template, fieldName, a.value());
+			                    fa.replace(formatTemplate);
+		                    }
+	                    } catch (NotFoundException e){}
+	                    catch (ClassNotFoundException e){}
 	                }
 	         	}
 	        });
 	    }
-	}
-
-	private String parseAssertion(CtClass ctClass, String name) {
-		try{
-			CtField ctField = ctClass.getField(name);
-			Assertion a = (Assertion) ctField.getAnnotation(Assertion.class);
-			return a.value();
-		} catch (NotFoundException e){
-			return null;
-		} catch (ClassNotFoundException e){
-			return null;
-		}
 	}
 
 	public static void checkField(boolean expr) {
